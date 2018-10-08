953e0c6acdf25e96e223dabe574de652134c32c2
xiaoqi <812817883@qq.com>
2018-10-08 16:55:28 +0800

    test1

--- app/src/main/java/com/noober/imagebrowser/MainActivity.java
+++ app/src/main/java/com/noober/imagebrowser/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         list.add("http://img1.imgtn.bdimg.com/it/u=3920398476,1501488149&fm=27&gp=0.jpg");
         list.add("http://img1.imgtn.bdimg.com/it/u=3920398476,1501488149&fm=27&gp=0.jpg");
         list.add("http://img1.imgtn.bdimg.com/it/u=3920398476,1501488149&fm=27&gp=0.jpg");list.add("http://img1.imgtn.bdimg.com/it/u=3920398476,1501488149&fm=27&gp=0.jpg");
-
+        //test1
         imageBrowser.setImageList(list);
     }
 }
